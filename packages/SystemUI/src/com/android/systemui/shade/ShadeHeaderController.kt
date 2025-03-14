157f631c117eeb6bdef4246671c8989092e831ee
Evan Laird <evanlaird@google.com>
2025-03-14 13:12:07 -0400

    [sb] use sp to define the status bar battery metrics
    Status bar icons scale with font size AND display density, and this
    change enables that for the new battery icon.
    
    Also updates the size and arrangement in the shade header to match
    status bar.
    
    Test: manual; change font scale and battery size updates
    Bug: 402608850
    Fixes: 400376762
    Fixes: 402059273
    Flag: com.android.settingslib.flags.new_status_bar_icons
    Flag: com.android.systemui.status_bar_root_modernization
    Change-Id: Ifbbdd959f8c6e00bf19348d13c04cdbfdff86abc

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -40,11 +40,10 @@ import android.view.WindowInsets
 import android.widget.TextView
 import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.view.doOnLayout
 import androidx.core.view.isVisible
@@ -386,7 +385,7 @@ constructor(
                         id = R.id.battery_meter_composable_view
                         val showBatteryEstimate by showBatteryEstimate.collectAsStateWithLifecycle()
                         BatteryWithEstimate(
-                            modifier = Modifier.height(17.dp).wrapContentWidth(),
+                            modifier = Modifier.wrapContentSize(),
                             viewModelFactory = batteryViewModelFactory,
                             isDark = { true },
                             showEstimate = showBatteryEstimate,
