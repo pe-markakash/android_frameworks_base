b62b3593703b9c510ff08f19f48b2fc0dbc1c13a
Nicolo' Mazzucato <nicomazz@google.com>
2025-04-02 14:22:56 +0000

    Fix shade context used by VPN bar in QS to create a dialog
    This is a temporary fix for the "VPN" dialog to make it use the shade dialog context instead of the plain QS one.
    
    Note this issue is already fixed by the
    com.android.window.flags.enable_window_context_override_type flag, but it will need some time to ramp up. This cl prevents crashes in the meantime.
    
    Bug: 362719719
    Bug: 407690449
    Test: QSSecurityFooterTest
    Flag: com.android.systemui.shade_window_goes_around
    Flag: com.android.window.flags.enable_window_context_override_type
    Change-Id: Ic02bed3672bdd2dbb227029f266033a601d013f5

--- packages/SystemUI/tests/utils/src/com/android/systemui/qs/QuickSettingsKosmos.kt
+++ packages/SystemUI/tests/utils/src/com/android/systemui/qs/QuickSettingsKosmos.kt
@@ -39,6 +39,7 @@ import com.android.systemui.qs.footer.foregroundServicesRepository
 import com.android.systemui.qs.footer.ui.viewmodel.FooterActionsViewModel
 import com.android.systemui.security.data.repository.securityRepository
 import com.android.systemui.settings.userTracker
+import com.android.systemui.shade.data.repository.shadeDialogContextInteractor
 import com.android.systemui.shade.domain.interactor.shadeModeInteractor
 import com.android.systemui.statusbar.policy.deviceProvisionedController
 import com.android.systemui.statusbar.policy.securityController
@@ -69,6 +70,7 @@ val Kosmos.qsSecurityFooterUtils by Fixture {
         securityController,
         looper,
         dialogTransitionAnimator,
+        shadeDialogContextInteractor,
     )
 }
 
