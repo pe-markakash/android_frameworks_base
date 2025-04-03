b62e922e8a7bab12642f3b570f397b837554c243
Steve Elliott <steell@google.com>
2025-04-03 18:29:45 -0400

    [kairos] keep shadecarriergroup view-model lookup up-to-date
    Flag: com.android.systemui.status_bar_mobile_icon_kairos
    Bug: 383172066
    Test: atest
    Change-Id: I851332f2e19676b615b21d5c7e362c819c6b3114

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/view/ModernShadeCarrierGroupMobileView.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/view/ModernShadeCarrierGroupMobileView.kt
@@ -20,9 +20,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
-import com.android.systemui.kairos.BuildSpec
 import com.android.systemui.kairos.ExperimentalKairosApi
 import com.android.systemui.kairos.KairosNetwork
+import com.android.systemui.kairos.buildSpec
 import com.android.systemui.res.R
 import com.android.systemui.statusbar.StatusBarIconView.STATE_ICON
 import com.android.systemui.statusbar.phone.StatusBarLocation
@@ -96,7 +96,7 @@ class ModernShadeCarrierGroupMobileView(context: Context, attrs: AttributeSet?) 
             context: Context,
             logger: MobileViewLogger,
             slot: String,
-            viewModel: BuildSpec<ShadeCarrierGroupMobileIconViewModelKairos>,
+            viewModel: ShadeCarrierGroupMobileIconViewModelKairos,
             scope: CoroutineScope,
             subscriptionId: Int,
             location: StatusBarLocation,
@@ -114,7 +114,7 @@ class ModernShadeCarrierGroupMobileView(context: Context, attrs: AttributeSet?) 
                         val (binding, _) =
                             MobileIconBinderKairos.bind(
                                 view = iconView,
-                                viewModel = viewModel,
+                                viewModel = buildSpec { viewModel },
                                 initialVisibilityState = STATE_ICON,
                                 logger = logger,
                                 scope = this,
@@ -126,7 +126,13 @@ class ModernShadeCarrierGroupMobileView(context: Context, attrs: AttributeSet?) 
 
                     val textView =
                         view.requireViewById<AutoMarqueeTextView>(R.id.mobile_carrier_text)
-                    launch { ShadeCarrierBinderKairos.bind(textView, viewModel, kairosNetwork) }
+                    launch {
+                        ShadeCarrierBinderKairos.bind(
+                            textView,
+                            buildSpec { viewModel },
+                            kairosNetwork,
+                        )
+                    }
                 }
         }
     }
