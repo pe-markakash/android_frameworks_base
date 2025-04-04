3d027fa24a4547e2736416388cbb24f63e22f404
Jason Zhang <jiajunz@google.com>
2025-04-04 21:41:17 +0000

    Add "Add network" button in Internet detailed view
    Adds the "Add network" button in internet detailed view in quick
    settings. Clicking on that will invoke WiFi configuration dialog.
    
    Demo: https://drive.google.com/file/d/1o147dj3id3f2o7rtd1G0a85b4ZwdCGns/view?usp=sharing&resourcekey=0-w_SBtaiZyDiN7dDDJh5whg
    
    Bug: 408503550
    Flag: com.android.systemui.qs_wifi_config
    Test: atest InternetDetailsContentManagerTest
    Change-Id: Id14f57fe63b9b9c41cee970cfb5408b0b1b76d14

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDetailsContentManager.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDetailsContentManager.kt
@@ -19,6 +19,7 @@ package com.android.systemui.qs.tiles.dialog
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Canvas
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
@@ -63,10 +64,12 @@ import com.android.internal.logging.UiEventLogger
 import com.android.settingslib.satellite.SatelliteDialogUtils.TYPE_IS_WIFI
 import com.android.settingslib.satellite.SatelliteDialogUtils.mayStartSatelliteWarningDialog
 import com.android.settingslib.wifi.WifiEnterpriseRestrictionUtils
+import com.android.settingslib.wifi.WifiUtils
 import com.android.systemui.Prefs
 import com.android.systemui.accessibility.floatingmenu.AnnotationLinkSpan
 import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.dagger.qualifiers.Main
+import com.android.systemui.qs.flags.QsWifiConfig
 import com.android.systemui.res.R
 import com.android.systemui.statusbar.phone.SystemUIDialog
 import com.android.systemui.statusbar.policy.KeyguardStateController
@@ -127,6 +130,7 @@ constructor(
     private lateinit var mobileDataToggle: MaterialSwitch
     private lateinit var wifiToggle: MaterialSwitch
     private lateinit var shareWifiButton: LinearLayout
+    private lateinit var addNetworkButton: LinearLayout
     private lateinit var airplaneModeButton: Button
     private var alertDialog: AlertDialog? = null
     private var canChangeWifiState = false
@@ -255,6 +259,19 @@ constructor(
                 )
             ) {
                 uiEventLogger.log(InternetDetailsEvent.SHARE_WIFI_QS_BUTTON_CLICKED)
+            }
+        }
+
+        // Add network
+        addNetworkButton = contentView.requireViewById(R.id.add_network_button)
+        if (QsWifiConfig.isEnabled) {
+            addNetworkButton.setOnClickListener {
+                val intent =
+                    WifiUtils.getWifiDialogIntent(null, true /* connectForCaller */).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+                    }
+                internetDetailsContentController.startActivityForDialog(intent)
             }
         }
 
@@ -459,6 +476,7 @@ constructor(
         wifiRecyclerView.visibility = View.GONE
         seeAllLayout.visibility = View.GONE
         shareWifiButton.visibility = View.GONE
+        addNetworkButton.visibility = View.GONE
     }
 
     private fun setProgressBarVisible(visible: Boolean) {
@@ -839,7 +857,11 @@ constructor(
         if (!internetContent.isWifiEnabled || internetContent.isDeviceLocked) {
             wifiRecyclerView.visibility = View.GONE
             seeAllLayout.visibility = View.GONE
+            addNetworkButton.visibility = View.GONE
             return
+        }
+        if (QsWifiConfig.isEnabled) {
+            addNetworkButton.visibility = View.VISIBLE
         }
         val wifiListMaxCount = getWifiListMaxCount()
         if (adapter.itemCount > wifiListMaxCount) {
@@ -935,6 +957,7 @@ constructor(
         wifiToggle.setOnCheckedChangeListener(null)
         mobileDataToggle.setOnCheckedChangeListener(null)
         shareWifiButton.setOnClickListener(null)
+        addNetworkButton.setOnClickListener(null)
         airplaneModeButton.setOnClickListener(null)
         internetDetailsContentController.onStop()
     }
