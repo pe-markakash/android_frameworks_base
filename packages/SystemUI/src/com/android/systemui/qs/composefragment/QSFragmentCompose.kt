a20613b9e60aacb94f37cb9a01402ab02679b076
Michael Mikhail <michaelmikhil@google.com>
2025-04-10 15:56:32 +0000

    Fix clipped inactive UMO on new QS design
    Flag: com.android.systemui.qs_ui_refactor_compose_fragment
    Bug: 404815434
    Test: checked UI.
    Change-Id: I4054d7e0f9f3e0545c75bddae4e19e52638f49b2

--- packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
+++ packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
@@ -155,6 +155,7 @@ import com.android.systemui.res.R
 import com.android.systemui.util.LifecycleFragment
 import com.android.systemui.util.animation.UniqueObjectHostView
 import com.android.systemui.util.asIndenting
+import com.android.systemui.util.children
 import com.android.systemui.util.printSection
 import com.android.systemui.util.println
 import com.android.systemui.util.settings.SystemSettings
@@ -1350,7 +1351,17 @@ private fun MediaObject(
                         )
                 }
             },
-            update = { view -> view.update() },
+            update = { view ->
+                view.update()
+                // Update layout params if host view bounds are higher than its child.
+                val height = mediaHost.hostView.height
+                val width = mediaHost.hostView.width
+                mediaHost.hostView.children.forEach { child ->
+                    if (child is FrameLayout && (height > child.height || width > child.width)) {
+                        child.layoutParams = FrameLayout.LayoutParams(width, height)
+                    }
+                }
+            },
             onReset = {},
         )
     }
