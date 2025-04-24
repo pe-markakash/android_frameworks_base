148e825ec173c0c627b8e2cd98a1654be330a984
Ale Nijamkin <nijamkin@google.com>
2025-04-24 13:13:47 -0700

    [flexiglass] Separates isBypassEnabled from DeviceEntryInteractor
    Creates a new DeviceEntryBypassInteractor and moves isBypassEnabled to
    it. Updates all usages of isBypassEnabled to use the new interactor.
    
    This should help us with Wear, where System UI crash loops because as
    soon as Dagger instantiates DeviceEntryInteractor, there are crashes
    that stem from dependencies held for isBypassEnabled.
    
    Bug: 412766066
    Test: unit tests updated and new ones creates for the new interactor and
    repository pair
    Test: manually verified that unlocking with face unlock respects the
    "Skip lockscreen" user setting (both ways)
    Flag: com.android.systemui.scene_container
    
    Change-Id: Iee523dd901aa253ecbb12902e2e79eadfc29717b

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/viewmodel/LockscreenContentViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/viewmodel/LockscreenContentViewModel.kt
@@ -21,7 +21,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import com.android.app.tracing.coroutines.launchTraced as launch
 import com.android.systemui.biometrics.AuthController
-import com.android.systemui.customization.R as customR
+import com.android.systemui.customization.clocks.R as clocksR
+import com.android.systemui.deviceentry.domain.interactor.DeviceEntryBypassInteractor
 import com.android.systemui.deviceentry.domain.interactor.DeviceEntryInteractor
 import com.android.systemui.keyguard.domain.interactor.KeyguardBlueprintInteractor
 import com.android.systemui.keyguard.domain.interactor.KeyguardClockInteractor
@@ -54,7 +55,7 @@ constructor(
     val touchHandling: KeyguardTouchHandlingViewModel,
     shadeModeInteractor: ShadeModeInteractor,
     unfoldTransitionInteractor: UnfoldTransitionInteractor,
-    deviceEntryInteractor: DeviceEntryInteractor,
+    deviceEntryBypassInteractor: DeviceEntryBypassInteractor,
     transitionInteractor: KeyguardTransitionInteractor,
     private val keyguardTransitionAnimationCallbackDelegator:
         KeyguardTransitionAnimationCallbackDelegator,
@@ -129,7 +130,7 @@ constructor(
     val isBypassEnabled: Boolean by
         hydrator.hydratedStateOf(
             traceName = "isBypassEnabled",
-            source = deviceEntryInteractor.isBypassEnabled,
+            source = deviceEntryBypassInteractor.isBypassEnabled,
         )
 
     val blueprintId: String by
