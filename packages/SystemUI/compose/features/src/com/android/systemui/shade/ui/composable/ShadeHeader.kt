adf2a30e0720b1862aaee1a90b1bdd9b3acb7f83
burakov <burakov@google.com>
2025-04-28 09:16:16 +0000

    [flexiglass] Ensure shade header height matches the status bar height.
    Bonus: Simplify the transition spec to the QuickSettings scene.
    
    Fix: 396405667
    Bug: 413766503
    Bug: 408486251
    Bug: 409594391
    Test: Manually by expanding the shade and observing the height matches
     that of the status bar, on both a folded and unfolded device.
    Flag: com.android.systemui.scene_container
    Change-Id: Id83813b8d58a556bb289d22d3f6429c2c59b20d3

--- packages/SystemUI/compose/features/src/com/android/systemui/shade/ui/composable/ShadeHeader.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/shade/ui/composable/ShadeHeader.kt
@@ -22,7 +22,6 @@ import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
@@ -30,14 +29,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
@@ -56,6 +58,7 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
@@ -82,10 +85,8 @@ import com.android.systemui.kairos.ExperimentalKairosApi
 import com.android.systemui.privacy.OngoingPrivacyChip
 import com.android.systemui.res.R
 import com.android.systemui.scene.shared.model.Scenes
-import com.android.systemui.shade.ui.composable.ShadeHeader.Colors.onScrimDim
 import com.android.systemui.shade.ui.composable.ShadeHeader.Dimensions.ChipPaddingHorizontal
 import com.android.systemui.shade.ui.composable.ShadeHeader.Dimensions.ChipPaddingVertical
-import com.android.systemui.shade.ui.composable.ShadeHeader.Dimensions.CollapsedHeight
 import com.android.systemui.shade.ui.composable.ShadeHeader.Values.ClockScale
 import com.android.systemui.shade.ui.viewmodel.ShadeHeaderViewModel
 import com.android.systemui.shade.ui.viewmodel.ShadeHeaderViewModel.HeaderChipHighlight
@@ -115,15 +116,18 @@ object ShadeHeader {
     }
 
     object Dimensions {
-        val CollapsedHeight = 48.dp
+        @Deprecated(
+            "Approximation of the collapsed shade header height, used in legacy shade transitions.",
+            replaceWith = ReplaceWith("StatusBarHeight"),
+        )
+        val CollapsedHeightForTransitions = 48.dp
         val ExpandedHeight = 120.dp
         val ChipPaddingHorizontal = 6.dp
         val ChipPaddingVertical = 4.dp
-    }
 
-    object Colors {
-        val ColorScheme.onScrimDim: Color
-            get() = Color.DarkGray
+        val StatusBarHeight: Dp
+            @Composable
+            get() = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
     }
 
     object TestTags {
@@ -154,7 +158,7 @@ fun ContentScope.CollapsedShadeHeader(
 
     // This layout assumes it is globally positioned at (0, 0) and is the same size as the screen.
     CutoutAwareShadeHeader(
-        modifier = modifier.sysuiResTag(ShadeHeader.TestTags.Root),
+        modifier = modifier,
         startContent = {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -172,12 +176,7 @@ fun ContentScope.CollapsedShadeHeader(
         },
         endContent = {
             if (isPrivacyChipVisible) {
-                Box(
-                    modifier =
-                        Modifier.height(CollapsedHeight)
-                            .fillMaxWidth()
-                            .padding(horizontal = horizontalPadding)
-                ) {
+                Box(modifier = Modifier.fillMaxSize().padding(horizontal = horizontalPadding)) {
                     PrivacyChip(
                         viewModel = viewModel,
                         modifier = Modifier.align(Alignment.CenterEnd),
@@ -233,7 +232,7 @@ fun ContentScope.ExpandedShadeHeader(
 
     Box(modifier = modifier.sysuiResTag(ShadeHeader.TestTags.Root)) {
         if (isPrivacyChipVisible) {
-            Box(modifier = Modifier.height(CollapsedHeight).fillMaxWidth()) {
+            Box(modifier = Modifier.height(ShadeHeader.Dimensions.StatusBarHeight).fillMaxWidth()) {
                 PrivacyChip(viewModel = viewModel, modifier = Modifier.align(Alignment.CenterEnd))
             }
         }
@@ -262,7 +261,7 @@ fun ContentScope.ExpandedShadeHeader(
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.element(ShadeHeader.Elements.ExpandedContent),
+                modifier = Modifier.element(ShadeHeader.Elements.ExpandedContent).fillMaxWidth(),
             ) {
                 VariableDayDate(
                     longerDateText = viewModel.longerDateText,
@@ -322,7 +321,7 @@ fun ContentScope.OverlayShadeHeaderPartialStateless(
 
     // This layout assumes it is globally positioned at (0, 0) and is the same size as the screen.
     CutoutAwareShadeHeader(
-        modifier = modifier.sysuiResTag(ShadeHeader.TestTags.Root),
+        modifier = modifier,
         startContent = {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -376,12 +375,7 @@ fun ContentScope.OverlayShadeHeaderPartialStateless(
                     )
                 }
                 if (isPrivacyChipVisible) {
-                    Box(
-                        modifier =
-                            Modifier.height(CollapsedHeight)
-                                .fillMaxWidth()
-                                .padding(horizontal = horizontalPadding)
-                    ) {
+                    Box(modifier = Modifier.fillMaxSize().padding(horizontal = horizontalPadding)) {
                         PrivacyChip(
                             viewModel = viewModel,
                             modifier = Modifier.align(Alignment.CenterEnd),
@@ -423,6 +417,7 @@ private fun CutoutAwareShadeHeader(
     val cutoutHeight = LocalDisplayCutout.current.height()
     val cutoutTop = LocalDisplayCutout.current.top
     val cutoutLocation = LocalDisplayCutout.current.location
+    val statusBarHeight = ShadeHeader.Dimensions.StatusBarHeight
 
     Layout(
         modifier = modifier.sysuiResTag(ShadeHeader.TestTags.Root),
@@ -435,7 +430,7 @@ private fun CutoutAwareShadeHeader(
 
         val screenWidth = constraints.maxWidth
         val cutoutWidthPx = cutoutWidth.roundToPx()
-        val height = max(cutoutHeight + (cutoutTop * 2), CollapsedHeight).roundToPx()
+        val height = max(cutoutHeight + (cutoutTop * 2), statusBarHeight).roundToPx()
         val childConstraints = Constraints.fixed((screenWidth - cutoutWidthPx) / 2, height)
 
         val startMeasurable = measurables[0][0]
@@ -744,16 +739,11 @@ private fun SystemIconChip(
         modifier =
             modifier
                 .clip(RoundedCornerShape(25.dp))
-                .thenIf(onClick != null) {
-                    Modifier.clickable(
-                        onClick = { onClick?.invoke() },
-                    )
-                }
+                .thenIf(onClick != null) { Modifier.clickable(onClick = { onClick?.invoke() }) }
                 .thenIf(backgroundColor != Color.Unspecified) {
                     Modifier.background(backgroundColor)
                         .padding(horizontal = ChipPaddingHorizontal, vertical = ChipPaddingVertical)
                 },
-
         content = content,
     )
 }
