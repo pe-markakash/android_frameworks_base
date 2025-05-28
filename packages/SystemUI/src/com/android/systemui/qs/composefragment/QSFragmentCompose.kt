2fa81bd6cb9730ec7441e2d9fdfb2e070941047d
Behnam Heydarshahi <bhnm@google.com>
2025-05-28 21:59:04 +0000

    Tiles wont start listening when overscrolling
    This will avoid startling and stopping the tile listening for QS
    tiles when flinging up from the end of many notifications.'
    
    Bug: 414346597
    Test: atest QSFragmentComposeTest
    Flag: EXEMPT bugfix
    Change-Id: Ib188ee6cd0805c3271f6de4a9a47887b4c77c64f

--- packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
+++ packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
@@ -882,7 +882,8 @@ constructor(
                                                             viewModel.expansionState.progress >
                                                                 QSFragmentComposeViewModel
                                                                     .QS_LISTENING_THRESHOLD &&
-                                                            !viewModel.isEditing
+                                                            !viewModel.isEditing &&
+                                                            !viewModel.isStackScrollerOverscrolling
                                                     }
                                                 }
                                                 .let { state -> { state.value } }
