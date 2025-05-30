40e23f6f48e3c0dee962b9c6c6db1cb8de464a38
Olivier St-Onge <ostonge@google.com>
2025-05-30 10:46:39 -0400

    Limit edit tiles title to 2 lines
    Flag: com.android.systemui.qs_ui_refactor_compose_fragment
    Fixes: 417612891
    Test: manually - setting display and font size to largest
    Change-Id: Ic058b49a88d61db4ef5c0ecadd7c43c1fa9a52f7

--- packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
@@ -196,6 +196,8 @@ private fun EditModeTopBar(onStopEditing: () -> Unit, onReset: (() -> Unit)?) {
                 text = stringResource(id = R.string.qs_edit_tiles),
                 style = MaterialTheme.typography.titleLargeEmphasized,
                 modifier = Modifier.padding(start = 24.dp),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 2,
             )
         },
         navigationIcon = {
